d1a0699829cd84f97039a8e678f98cae717006f0
Etson <etsond@ufl.edu>
2023-01-22 19:42:00 -0500

    working on dates

--- script.js
+++ script.js
@@ -332,3 +332,12 @@ btnSort.addEventListener("click", function(e) {
 
 
 
+const future =  new Date(2037, 10, 19,15,23);
+console.log(+future)
+
+const calcDaysPassed = (date1, date2) => (date2 - date1) / (1000 * 60 *60 *24)
+
+calcDaysPassed(new Date(2037, 3,14),new Date(2037, 3,24) );
+
+console.log(day1);
+
